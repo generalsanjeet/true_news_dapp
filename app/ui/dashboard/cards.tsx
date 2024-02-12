e615b0de39227cf8aefd0390e598fc424bdaf7cc
generalsanjeet <generalsanjeet@gmail.com>
2024-02-12 12:52:47 +0530

    some ui changes

--- app/ui/dashboard/cards.tsx
+++ app/ui/dashboard/cards.tsx
@@ -50,17 +50,18 @@ export function Card({
   const Icon = iconMap[type];
 
   return (
-    <div className="rounded-xl bg-gray-50 p-2 shadow-sm">
+    <div className="rounded-xl bg-black p-2 shadow-sm">
       <div className="flex p-4">
-        {Icon ? <Icon className="h-5 w-5 text-gray-700" /> : null}
-        <h3 className="ml-2 text-sm font-medium">{title}</h3>
+        {Icon ? <Icon className="h-5 w-5 text-amber-500" /> : null}
+        <h3 className="ml-2 text-sm font-medium text-amber-500">{title}</h3>
       </div>
       <p
         className={`${lusitana.className}
-          truncate rounded-xl bg-white px-4 py-8 text-center text-2xl`}
+          truncate rounded-xl bg-stone-800 px-4 py-8 text-center text-2xl text-white`}
       >
         {value}
       </p>
     </div>
   );
 }
+
