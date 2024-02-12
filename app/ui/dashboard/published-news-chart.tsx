c5b790b6b12235cb26dee5e411574f449a5ea40a
generalsanjeet <generalsanjeet@gmail.com>
2024-02-12 13:53:58 +0530

    ui changes in chart done

--- app/ui/dashboard/published-news-chart.tsx
+++ app/ui/dashboard/published-news-chart.tsx
@@ -19,10 +19,9 @@ export default async function PublishedNewsChart() {
       <h2 className={`${lusitana.className} mb-4 text-xl md:text-2xl text-yellow-500`}>
         Recent Published News
       </h2>
-      {/* NOTE: comment in this code when you get to this point in the course */}
 
-       <div className="rounded-xl bg-gray-50 p-4">
-        <div className="sm:grid-cols-13 mt-0 grid grid-cols-12 items-end gap-2 rounded-md bg-white p-4 md:gap-4">
+       <div className="rounded-xl bg-stone-900 p-4">
+        <div className="sm:grid-cols-13 mt-0 grid grid-cols-12 items-end gap-2 rounded-md bg-stone-700 p-4 md:gap-4">
           <div
             className="mb-6 hidden flex-col justify-between text-sm text-gray-400 sm:flex"
             style={{ height: `${chartHeight}px` }}
@@ -35,20 +34,20 @@ export default async function PublishedNewsChart() {
           {publishedNews.map((month) => (
             <div key={month.month} className="flex flex-col items-center gap-2">
               <div
-                className="w-full rounded-md bg-blue-300"
+                className="w-full rounded-md bg-stone-500"
                 style={{
                   height: `${(chartHeight / topLabel) * month.total}px`,
                 }}
               ></div>
-              <p className="-rotate-90 text-sm text-gray-400 sm:rotate-0">
+              <p className="-rotate-90 text-sm text-white sm:rotate-0">
                 {month.month}
               </p>
             </div>
           ))}
         </div>
         <div className="flex items-center pb-2 pt-6">
-          <CalendarIcon className="h-5 w-5 text-gray-500" />
-          <h3 className="ml-2 text-sm text-gray-500 ">Last 12 months</h3>
+          <CalendarIcon className="h-5 w-5 text-amber-500" />
+          <h3 className="ml-2 text-sm text-amber-500">Last 12 months</h3>
         </div>
       </div> 
     </div>
