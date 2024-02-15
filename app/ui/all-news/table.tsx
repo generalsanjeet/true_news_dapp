72f46ba10c689625671d0142cb0ec38345afc3be
generalsanjeet <generalsanjeet@gmail.com>
2024-02-15 05:41:04 +0530

    style changes in mobile view

--- app/ui/all-news/table.tsx
+++ app/ui/all-news/table.tsx
@@ -20,7 +20,7 @@ export default async function AllNewsTable({
         <div className="rounded-lg bg-stone-900 p-2 md:pt-0">
           <div className="md:hidden">
             {all_news?.map((news) => (
-              <div key={news.id} className="mb-2 w-full rounded-md bg-white p-4">
+              <div key={news.id} className="mb-2 w-full rounded-md bg-stone-700 p-4 text-white">
                 <div className="flex items-center justify-between border-b pb-4">
                   <div>
                     <div className="mb-2 flex items-center">
@@ -33,7 +33,7 @@ export default async function AllNewsTable({
                       />
                       <p>{news.name}</p>
                     </div>
-                    <p className="text-sm text-gray-500">{news.email}</p>
+                    <p className="text-sm text-white">{news.email}</p>
                   </div>
                   <PubOnBlock published_on_blockchain={news.published_on_blockchain} />
                 </div>
@@ -79,7 +79,7 @@ export default async function AllNewsTable({
               {all_news?.map((news) => (
                 <tr
                   key={news.id}
-                  className="w-full border-b py-3 text-sm last-of-type:border-none [&:first-child>td:first-child]:rounded-tl-lg [&:first-child>td:last-child]:rounded-tr-lg [&:last-child>td:first-child]:rounded-bl-lg [&:last-child>td:last-child]:rounded-br-lg"
+                  className="w-full border-b py-3 text-sm last-of-type:border-none [&:first-child>td:first-child]:rounded-tl-lg [&:first-child>td:last-child]:rounded-tr-lg [&:last-child>td:first-child]:rounded-bl-lg [&:last-child>td:last-child]:rounded-br-lg text-white"
                 >
                   <td className="whitespace-nowrap py-3 pl-6 pr-3">
                     <div className="flex items-center gap-3">
@@ -93,13 +93,13 @@ export default async function AllNewsTable({
                       <p>{news.name}</p>
                     </div>
                   </td>
-                  <td className="whitespace-nowrap px-3 py-3 text-gray-400">
+                  <td className="whitespace-nowrap px-3 py-3 text-stone-400">
                     {news.email}
                   </td>
-                  <td className="whitespace-nowrap px-3 py-3">
+                  <td className="whitespace-nowrap px-3 py-3 text-white">
                     {news.headline}
                   </td>
-                  <td className="whitespace-nowrap px-3 py-3">
+                  <td className="whitespace-nowrap px-3 py-3 text-white">
                     {formatDateToLocal(news.date)}
                   </td>
                   <td className="whitespace-nowrap px-3 py-3">
