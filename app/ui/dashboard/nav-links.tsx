908ce5d5075fb6100772934471964b6a0a62e10c
generalsanjeet <generalsanjeet@gmail.com>
2024-02-15 20:49:01 +0530

    some button hover effect changes made

--- app/ui/dashboard/nav-links.tsx
+++ app/ui/dashboard/nav-links.tsx
@@ -31,7 +31,7 @@ export default function NavLinks() {
           <Link
             key={link.name}
             href={link.href}
-            className={clsx( 'flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-stone-600 p-3 text-sm font-medium hover:bg-stone-500 text-gray-300 hover:text-white md:flex-none md:justify-start md:p-2 md:px-3', { 'bg-stone-500 text-whie': pathname === link.href, },)}>
+            className={clsx( 'flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-stone-600 p-3 text-sm font-medium hover:bg-stone-700 text-stone-400 hover:text-white md:flex-none md:justify-start md:p-2 md:px-3', { 'bg-stone-700 text-white': pathname === link.href, },)}>
             <LinkIcon className="w-6" />
             <p className="hidden md:block">{link.name}</p>
           </Link>
