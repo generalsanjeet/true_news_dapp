cd8905af0996216df0450428a4353a583b8422c7
generalsanjeet <generalsanjeet@gmail.com>
2024-02-17 05:46:37 +0530

    changes in newsviews component

--- app/ui/all-news/news-view.tsx
+++ app/ui/all-news/news-view.tsx
@@ -11,11 +11,25 @@ export default function NewsView({
 }: {
     newsview: NewsViewType;
 }) {
-    const sampleImageUrl = 'https://picsum.photos/200/300?grayscale';
+    console.log("newsview data is ", newsview);
+
     return (
         <div className="bg-stone-800 p-4 md:p-6 h-auto w-auto ">
+            <div className="flex items-center text-white text-lg mt-5">
+                <div className="mb-2 flex items-center">
+                  <Image
+                    src={newsview.image_url}
+                    className="mr-2 rounded-full"
+                    width={28}
+                    height={28}
+                    alt={`${newsview.name}'s profile picture`}
+                  />
+                  <p>{newsview.name}</p>
+                </div>
+            </div>
+
             <div className='mb-5  text-center '>
-                <h1 className="text-2xl font-bold  text-amber-500 uppercase underline shadow-sm">{newsview.headline}</h1>
+                <h1 className="text-2xl font-bold  text-yellow-500 uppercase  shadow-sm">{newsview.headline}</h1>
             </div>
 
             <div className="flex items-center justify-center bg-black rounded-md p-5">
@@ -24,14 +38,12 @@ export default function NewsView({
                 />
             </div>
 
+
             <div className="mb-4">
+                <NewspaperIcon className="h-5 w-5 mr-1" />
                 <p className="text-gray-600">{newsview.content}</p>
             </div>
 
-            <div className="flex items-center text-gray-500">
-                <NewspaperIcon className="h-5 w-5 mr-1" />
-                <span>Channel: {newsview.name}</span>
-            </div>
 
             <div className="mt-4">
                 <h2 className="text-sm font-medium text-gray-500">Published on blockchain:</h2>
